ae70fdefbbc6a58f2016678b32c390f93f3d7171
EddyEkko <edward.agboraw@gmail.com>
2021-10-26 14:36:02 +0200

    Add files via upload

---
+++ Code/app.R
@@ -0,0 +1,11 @@
+library(shiny)
+
+
+source('ui.R')
+source('server.R')
+
+
+shinyApp(
+  ui = ui_2,
+  server = server
+)
---
+++ Code/install_packages.R
@@ -0,0 +1,32 @@
+#Package Installation:
+
+#General Framework
+install.packages("shiny")
+install.packages("Seurat")
+install.packages("shinythemes")
+install.packages("shinycssloaders")
+install.packages("magrittr")
+install.packages("SingleCellExperiment")
+
+#For Feature Selection
+install.packages("DUBStepR")
+install.packages("M3Drop")
+
+#For Dimension Reduction
+
+install.packages("VGAM")
+install.packages("SeuratWrappers")
+install.packages("scry")
+
+#For Cluster Validation
+install.packages("NbClust")
+install.packages("ggplot2")
+install.packages("factoextra")
+
+#For Cluster Analysis
+install.packages("HGC")
+install.packages("monocle")
+install.packages("SC3")
+install.packages("scDHA")
+install.packages("bluster")
+install.packages("dynamicTreeCut")
